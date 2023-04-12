7f6b1adcf08d29f2e0724232bcc8fddd420ab4aa
AngeCk98 <Angekoubangou@gmail.com>
2023-04-12 00:55:10 -0700

    Task 0-10

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+let count = 0;
+exports.logMe = function (item) {
+  // Prints the number of arguments already printed and the new argument value
+  console.log(`${count++}: ${item}`);
+};
